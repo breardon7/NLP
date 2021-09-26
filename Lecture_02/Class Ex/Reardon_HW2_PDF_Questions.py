e5011c15ceb3cdc4c6a725b8b9a3e4ddfca55969
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-09-25 22:05:18 -0400

    Update Reardon_HW2_PDF_Questions.py

--- Lecture_02/Class Ex/Reardon_HW2_PDF_Questions.py
+++ Lecture_02/Class Ex/Reardon_HW2_PDF_Questions.py
@@ -10,9 +10,10 @@ print('token count: ', len(tokens))
 # iii.
 print('unique token count: ', len(set(tokens)))
 # iv
-from nltk.stem import WordNetLemmatizer
+'''from nltk.stem import WordNetLemmatizer
+verbs = [w for w in tokens if w.]
 wnl = WordNetLemmatizer()
-#print([wnl.lemmatize(t) for t in tokens])
+#print([wnl.lemmatize(t) for t in tokens])'''
 # v.
 # vi.
 whale = tokens.count('whale') + tokens.count('Whale')
@@ -22,7 +23,12 @@ from nltk import FreqDist
 freq_20 = FreqDist(tokens).most_common(20)
 print(freq_20)
 # viii.
+gr_six = [w for w in tokens if len(w) > 6]
+freq_160 = [k for (k,v) in FreqDist(gr_six).items() if v > 160]
+print(freq_160)
 # ix.
+sorted = tokens.sort(key=len, reverse=True)
+print(sorted)
 # x.
 # xi.
 # xii.
