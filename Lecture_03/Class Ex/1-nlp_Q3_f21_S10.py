41900c5fd735e8fb852de4dbb25d2c5ed52a6e41
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-10-01 16:53:33 -0400

    quiz3

--- Lecture_03/Class Ex/1-nlp_Q3_f21_S10.py
+++ Lecture_03/Class Ex/1-nlp_Q3_f21_S10.py
@@ -15,7 +15,8 @@ for token in doc:
 
 
 # -------------- Part of speech tags--------------------------------------
-
+text = open('clean_data.txt').read()
+doc = nlp(text)
 for token in doc[0:100]:
     print(token.text, token.pos_)
 
@@ -25,7 +26,7 @@ for token in doc[0:100]:
 
 
 for ent in doc.ents:
-    if ent.label_ == DATE:
+    if ent.label_ == 'DATE' or ent.label_ == 'TIME':
         print(ent)
 
 
@@ -39,12 +40,26 @@ for token in doc:
 
 
 
-
 # ----------------Document Similarities between ech sentences----------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+text2 = open('data1.txt', 'r').read()
 
-'''text2 = open('data1.txt').read()
-print(text.similarity(text2))'''
+doc = nlp(text2)
+sentences = [s for s in doc.sents]
 
+list2 = []
+for sent in doc.sents:
+    list1 = []
+    for sent2 in doc.sents:
+        list1.append(sent.similarity(sent2))
+    list2.append(list1)
+sim_array = np.array(list2)
+print(sim_array)
+print(list2)
 
-
-
+sns.set(font_scale = 4)
+plt.figure(figsize=(100,75))
+sns.heatmap(sim_array)
+plt.show()
