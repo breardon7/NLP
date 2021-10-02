7d0442fc1b22e29d44abd8869b8c6c1062bbe11a
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-10-02 11:36:10 -0400

    Update Reardon_HW3_PDF_Questions.py

--- Lecture_03/Class Ex/Reardon_HW3_PDF_Questions.py
+++ Lecture_03/Class Ex/Reardon_HW3_PDF_Questions.py
@@ -35,17 +35,43 @@ for chunk in doc.noun_chunks:
             chunk.root.head.text)
 # v.
 for token in doc:
-    print(token.text, token.dep_, token.head.text)
+    print(token.text, token.pos_, token.dep_, token.head.text)
 # vi.
-
+#nlp = spacy.load("en_core_web_trf")
+doc = nlp(string)
+for token in doc:
+    for token2 in doc:
+        print(token, '|', token2, ':', token.similarity(token2))
 # E.2
 # redacted means fill Names with character (i.e Josh ---> ****)
 # i.
+import pandas as pd
+df = pd.read_csv('data1.csv')
+print(df.columns)
 # ii.
+text = df['text'][5]
+doc = nlp(text)
+for ent in doc.ents:
+    print(ent.text, ent.label_)
 # iii.
 
 # E.2
 # i.
+text = 'This is a test sentence.'
+doc = nlp(text)
+for token in doc:
+    print(token.text, token.pos_)
 # ii.
+for token in doc:
+    print(token.text, token.dep_)
 # iii.
+text = 'Apple is looking at buying U.K. startup for 1 billion dollar.'
+doc = nlp(text)
+for ent in doc.ents:
+    print(ent.text, ent.label_)
 # iv.
+sent1 = 'This is a test sentence.'
+sent2 = 'Apple is looking at buying U.K. startup for 1 billion dollar.'
+doc1 = nlp(sent1)
+doc2 = nlp(sent2)
+print(doc1.similarity(doc2))
