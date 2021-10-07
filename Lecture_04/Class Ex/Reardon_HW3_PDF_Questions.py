1d12b9ec8eaf0dbc90a3c34df724a1a9e9b8fd50
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-10-07 13:37:15 -0400

    HW5

---
+++ Lecture_04/Class Ex/Reardon_HW3_PDF_Questions.py
@@ -0,0 +1,25 @@
+print(20*'-' + 'Begin E1' + 20*'-')
+
+# i
+# ii
+# iii
+
+print(20*'-' + 'End E1' + 20*'-')
+print(20*'-' + 'Begin E2' + 20*'-')
+
+# i
+# ii
+# iii
+
+
+print(20*'-' + 'End E2' + 20*'-')
+print(20*'-' + 'Begin E3' + 20*'-')
+# i
+# ii
+# iii
+# iv
+# v
+# vi
+# vii
+
+print(20*'-' + 'End E3' + 20*'-')
