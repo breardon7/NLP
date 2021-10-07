e0b0a7410e50502478bf1f340363c926817b68a6
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-10-07 15:27:37 -0400

    HW5

--- Lecture_04/Class Ex/Reardon_HW3_PDF_Questions.py
+++ Lecture_04/Class Ex/Reardon_HW3_PDF_Questions.py
@@ -1,19 +1,38 @@
 print(20*'-' + 'Begin E1' + 20*'-')
 
 # i
+file =  open('Email.txt', 'r')
+text = file.read()
+file.close()
 # ii
+import re
+pattern = re.compile(r'[a-zA-Z0-9._-]+@[a-zA-z]+\.(com|edu|net)')
+matches = pattern.finditer(text)
+for match in matches:
+    print(match)
 # iii
-
+print(text)
 print(20*'-' + 'End E1' + 20*'-')
 print(20*'-' + 'Begin E2' + 20*'-')
 
 # i
+file = open('war_and_peace.txt', 'r')
+text = file.read()
 # ii
+from collections import Counter
+pattern = re.compile(r'[A-Z][a-zA-Z]+ski')
+matches = pattern.finditer(text)
+list = []
+for match in matches:
+    list += [match[0]]
+print(Counter(list))
+
 # iii
 
 
 print(20*'-' + 'End E2' + 20*'-')
 print(20*'-' + 'Begin E3' + 20*'-')
+
 # i
 # ii
 # iii
