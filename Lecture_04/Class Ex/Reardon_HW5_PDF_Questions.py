3ecfb1d01845f68df274cdb9d06602a194c8d5e9
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-10-09 20:26:57 -0400

    Update Reardon_HW5_PDF_Questions.py

--- Lecture_04/Class Ex/Reardon_HW5_PDF_Questions.py
+++ Lecture_04/Class Ex/Reardon_HW5_PDF_Questions.py
@@ -22,16 +22,15 @@ text = file.read()
 from collections import Counter
 pattern = re.compile(r'[A-Z][a-zA-Z]+ski')
 matches = pattern.finditer(text)
-list = []
 for match in matches:
-    print(match)
-
+    print(match[0])
 # iii
+pattern = re.compile(r'[A-Z][a-zA-Z]+ski')
+matches = pattern.finditer(text)
 list = []
 for match in matches:
     list += [match[0]]
 print(Counter(list))
-
 print(20*'-' + 'End E2' + 20*'-')
 print(20*'-' + 'Begin E3' + 20*'-')
 
@@ -47,9 +46,23 @@ for match in matches:
     text = re.sub(r'\([a-zA-Z0-9]+\)', '('+'x'*length+')', text)
 print(text)
 # iii
+text = 'happily mad angry somethingly tragically'
+pattern = re.compile(r'[a-zA-Z]+ly')
+matches = pattern.finditer(text)
+for match in matches:
+    print(match)
 # iv
+text = '"Hello" "World" Not this one'
+pattern = re.compile(r'(["])(?:(?=(\\?))\2.)*?\1')
+matches = pattern.finditer(text)
+for match in matches:
+    print(match)
 # v
+text = 'short a the bradley'
+print(re.findall(r'\b\w{3,5}\b', text))
 # vi
+text = 'dfgjn,sdf,sdf'
+print(re.sub(r',', '-', text))
 # vii
 
 print(20*'-' + 'End E3' + 20*'-')
