905c083e7e31621cb496c08aad814e1ea22ea5bd
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-10-14 16:27:12 -0400

    Update Class-Ex-Lecture5.py

--- Lecture_05/Class Ex/Class-Ex-Lecture5.py
+++ Lecture_05/Class Ex/Class-Ex-Lecture5.py
@@ -21,7 +21,7 @@ print(20*'-' + 'End Q1' + 20*'-')
 
 # =================================================================
 # Class_Ex2:
-# Use the following datframe as the sample data.
+# Use the following dataframe as the sample data.
 # Find the conditional probability occurrence of thw word given a sentiment.
 # ----------------------------------------------------------------
 print(20*'-' + 'Begin Q2' + 20*'-')
@@ -51,14 +51,22 @@ print(20*'-' + 'End Q2' + 20*'-')
 # 4- Explain your results very carefully.
 # ----------------------------------------------------------------
 print(20*'-' + 'Begin Q3' + 20*'-')
+# import packages
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+# read in data
 df = pd.read_csv('data.csv')
 df['cleaned_text'] = ''
-#df['vectorized'] =
+vc = TfidfVectorizer()
 ps = PorterStemmer()
+
+# preprocess text data
 for i in range(len(df['text'])):
     tokens = word_tokenize(df['text'][i])
     lower = [token.lower() for token in tokens if token.isalnum()]
@@ -66,12 +74,34 @@ for i in range(len(df['text'])):
     stemmed = [ps.stem(token) for token in no_stop_words]
     cleaned_string = ' '.join(stemmed)
     df['cleaned_text'][i] = cleaned_string
+
+# Vectorize text data
 corpus = [row for row in df['cleaned_text']]
-print(corpus)
-print(df.head())
+vectorized_data = vc.fit_transform(corpus)
+#print(vectorized_data.shape)
 
+# Create train set
+X = vectorized_data
+y = df['label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+clf = MultinomialNB(alpha=0.5)
+clf.fit(X_train,y_train)
 
+# Test on test set
+predicted = clf.predict(X_test)
 
+# Classification report
+print(metrics.classification_report(y_test, predicted))
+print(metrics.confusion_matrix(y_test, predicted))
+
+# Exam code
+'''
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+clf = MultinomialNB()
+clf.fit(X_train,y_train)
+
+df_test['labels'] = clf.predict(insert df_test vectorized data)'''
+#df_test.to_csv(r'C:\Users\brear\OneDrive\Desktop\NLP\Exam1\HW5_Test_Output')
 
 
 
